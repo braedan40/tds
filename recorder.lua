b702aeb250a3381d4c8cd88dec96e2b6135c3f79
loomi <105733463+fileagent@users.noreply.github.com>
2025-02-01 20:49:57 +0100

    Update recorder.lua

--- recorder.lua
+++ recorder.lua
@@ -13,3 +13,30 @@ KillingCheats:CreateButton("Auto Skip", function()
 print("HI")
 end)
 
+local MessageBox = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/NotificationGUI/main/source.lua"))()
+
+
+--[[
+   MessageBoxIcons:
+      • Question
+      • Error
+      • Warning
+
+   MessageBoxButtons:
+      • YesNo
+      • OKCancel
+      • OK
+--]]
+-- AnchorPoint is 0.5,0.5
+MessageBox.Show({
+    Position = UDim2.new(0.5, 0, 0.5, 0),
+    Text = "Notification UI",
+    Description = "Recorder CW X  | Successfully Loaded ✅\nMade by : Wolny/Constance\n",
+    MessageBoxIcon = "Warning",
+    MessageBoxButtons = "OK",
+    Result = function(res)
+    end
+            })
+
+
+
