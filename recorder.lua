af01a7c93a75ee55de3e242f1ae3034772f4421b
loomi <105733463+fileagent@users.noreply.github.com>
2025-02-01 20:36:25 +0100

    Update recorder.lua

--- recorder.lua
+++ recorder.lua
@@ -1,1 +1,15 @@
 
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+local PhantomForcesWindow = Library:NewWindow("CW X | Recorder")
+
+local KillingCheats = PhantomForcesWindow:NewSection("Functions")
+
+KillingCheats:CreateToggle("Record", function(value)
+print(value)
+end)
+
+KillingCheats:CreateButton("Auto Skip", function()
+print("HI")
+end)
+
