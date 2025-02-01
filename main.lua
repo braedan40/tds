4b57bcedcf5d3d7a604c52c1ffc51739a3504a5b
braedan40 <braedan40@boringity.com>
2025-02-02 01:55:36 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -18,7 +18,124 @@ function waitwavetimer(wave, Timer)
 end
 
 functions.Map = function(MapName)
-    -- Implement this function
+   if inlobby() then
+
+
+        if not getgenv().Matchmaking then
+
+            if getgenv().legitmode == true then
+                for _, elevator in pairs(game:GetService("Workspace").Elevators:GetChildren()) do
+                    local elevatorMap = elevator:GetAttribute("Map")
+                    local playerCount = elevator:GetAttribute("Players")
+                    local elevatorType = elevator:GetAttribute("Type")
+
+
+                    if table.find(Map, elevatorMap)
+                       and playerCount < 1
+                       and elevator:FindFirstChild("Touch")
+                       and elevatorType == Type
+                    then
+                        moveTo(elevator.Touch.Position)
+                    elseif playerCount > 2 then
+                        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
+                            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(15, 6, 23.2)
+
+                        -- This call “Leave”
+                        game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Elevators"):WaitForChild("Leave"):InvokeServer()
+                    end
+                end
+
+            else
+
+                for _, elevator in pairs(game:GetService("Workspace").Elevators:GetChildren()) do
+                    local elevatorMap = elevator:GetAttribute("Map")
+                    local playerCount = elevator:GetAttribute("Players")
+
+                    if table.find(Map, elevatorMap) and playerCount < 1 then
+                        ReplicatedStorage:WaitForChild("Network"):WaitForChild("Elevators"):WaitForChild("RF:Enter"):InvokeServer(elevator)
+                    elseif playerCount > 2 then
+                        ReplicatedStorage:WaitForChild("Network"):WaitForChild("Elevators"):WaitForChild("RF:Leave"):InvokeServer()
+                    end
+                end
+            end
+
+
+        elseif getgenv().Matchmaking then
+            if inlobby() then
+
+                RemoteFunction:InvokeServer("Multiplayer", "single_create")
+                local args = {
+                [1] = "Multiplayer",
+                [2] = "v2:start",
+                [3] = {
+                ["difficulty"] = mode,
+                ["mode"] = Type,
+                ["count"] = 1
+             }
+         }
+
+RemoteFunction:InvokeServer(unpack(args))
+
+            elseif ingame() then
+                for i = 1, 4 do
+
+                end
+
+                repeat
+                    game:GetService("RunService").Heartbeat:Wait()
+                until game:GetService("Workspace"):FindFirstChild("IntermissionLobby")
+
+
+                for i = 1, 4 do
+                    local assignedMap = game:GetService("Workspace"):GetAttribute("Map" .. i)
+                    if assignedMap and table.find(Map, assignedMap) then
+                        moveTo(game:GetService("Workspace").IntermissionLobby.Boards.Board[i].Hitboxes.VotePlatform.Position)
+                        local args = {
+                            [1] = "LobbyVoting",
+                            [2] = "Vote",
+                            [3] = Map,
+                            [4] = game:GetService("Workspace").IntermissionLobby.Boards.Board[i].Hitboxes.VotePlatform.Position
+                        }
+                        ReplicatedStorage:WaitForChild("RemoteEvent"):FireServer(unpack(args))
+                    else
+                        RemoteEvent:FireServer("LobbyVoting", "Veto")
+                        game:GetService("RunService").Heartbeat:Wait(5)
+
+                        local newMapCheck = game:GetService("Workspace"):GetAttribute("Map" .. i)
+                        if newMapCheck and table.find(Map, newMapCheck) then
+                            moveTo(game:GetService("Workspace").IntermissionLobby.Boards.Board[i].Hitboxes.VotePlatform.Position)
+                            local args = {
+                                [1] = "LobbyVoting",
+                                [2] = "Vote",
+                                [3] = Map,
+                                [4] = game:GetService("Workspace").IntermissionLobby.Boards.Board[i].Hitboxes.VotePlatform.Position
+                            }
+                            ReplicatedStorage:WaitForChild("RemoteEvent"):FireServer(unpack(args))
+                        else
+                            RemoteFunction:InvokeServer("Multiplayer", "single_create")
+                        local args = {
+                [1] = "Multiplayer",
+                [2] = "v2:start",
+                [3] = {
+                ["difficulty"] = mode,
+                ["mode"] = Type,
+                ["count"] = 1
+             }
+         }
+
+RemoteFunction:InvokeServer(unpack(args))
+-- yeah go back to matchmaking again or i can made it leave
+                        end
+                    end
+                end
+            end
+        end
+
+
+    elseif ingame() then
+    if game:GetService("ReplicatedStorage").State.Map == Map then
+        return print("Map Matched..") -- yeah for function.place
+    end
 end
 
 functions.Place = function(Tower,wave,Timer)
